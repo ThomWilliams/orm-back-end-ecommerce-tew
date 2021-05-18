17019570ee7c1eb7ae6fb134662864883ed3987d
Thom Williams <thomwilliams1990@gmail.com>
2021-05-18 21:44:35 +0100

    Product Class added

--- models/Product.js
+++ models/Product.js
@@ -9,49 +9,38 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
-  //   * `Product`
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
 
-  // * `id`
-  
-  //   * Integer.
-  
-  //   * Doesn't allow null values.
-  
-  //   * Set as primary key.
-  
-  //   * Uses auto increment.
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
 
-  // * `product_name`
-  
-  //   * String.
-  
-  //   * Doesn't allow null values.
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
 
-  // * `price`
-  
-  //   * Decimal.
-  
-  //   * Doesn't allow null values.
-  
-  //   * Validates that the value is a decimal.
-
-  // * `stock`
-  
-  //   * Integer.
-  
-  //   * Doesn't allow null values.
-  
-  //   * Set a default value of `10`.
-  
-  //   * Validates that the value is numeric.
-
-  // * `category_id`
-  
-  //   * Integer.
-  
-  //   * References the `Category` model's `id`.
-
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      modelName: 'category', 
+    }
   },
   {
     sequelize,
